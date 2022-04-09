161912fcf31a78b1816682b32ea9a2c701854394
neverd1m <neverd1m@ya.ru>
2022-04-09 21:33:14 +0300

    Добавляю пакеты для тестов

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cd /home/yc-user
-sudo apt-get install ca-certificates -y
+sudo apt-get install ca-certificates apt-transport-https -y
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 
